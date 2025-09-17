1029fa2d71b82bb2c9ce6c9c0097032ce69dfee0
jafxfar <jafarjamshedi5@gmail.com>
2025-09-17 15:29:34 +0500

    public

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,6 @@
-export { auth as GET, auth as POST } from '@/auth';
+import NextAuth from 'next-auth';
+import { authConfig } from '@/auth.config';
+
+const handler = NextAuth(authConfig);
+
+export { handler as GET, handler as POST };
