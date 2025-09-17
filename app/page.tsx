9d137257179d0c6e7176beac886952808912bf50
jafxfar <jafarjamshedi5@gmail.com>
2025-09-17 13:51:05 +0500

    linter && client-side validation

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className={styles.shape}></div>
-      {/* <AcmeLogo /> */}
+      <AcmeLogo />
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-3xl font-bold md:text-4xl`}>
